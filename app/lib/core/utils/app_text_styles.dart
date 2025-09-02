e265d06dd80206c350e6c8a956f688eb190317e6
radwaadel17 <radwaadel959@gmail.com>
2025-09-02 22:23:21 +0300

    feat(styles): add AppTextStyles class with various text style definitions

---
+++ app/lib/core/utils/app_text_styles.dart
@@ -0,0 +1,72 @@
+import 'dart:ui';
+
+abstract class AppTextStyles {
+   static TextStyle bold13 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 13,
+  );
+  static TextStyle bold23 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 23,
+  );
+
+  static TextStyle semiBold13 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+  );
+
+  static TextStyle regular13 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 13,
+  );
+
+  static TextStyle bold16 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+  );
+
+  static TextStyle bold19 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 19,
+  );
+
+  static TextStyle semiBold16 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+
+  static TextStyle bold28 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 28,
+  );
+
+  static TextStyle regular22 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 22,
+  );
+
+  static TextStyle semiBold11 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 11,
+  );
+
+  static TextStyle medium15 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 15,
+  );
+
+  static TextStyle regular26 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 26,
+  );
+
+  static TextStyle regular16 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 16,
+  );
+
+  static TextStyle regular11 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 11,
+  );
+}
