7273e105d3a256ab4df77cda2193bd8c5c03b9d7
radwaadel17 <radwaadel959@gmail.com>
2025-09-09 21:26:31 +0300

    Refactor code structure for improved readability and maintainability

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,6 +14,9 @@ class FruitApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Cairo',
+      ),
        localizationsDelegates: const [
                 S.delegate,
                 GlobalMaterialLocalizations.delegate,
