cccdc1f4c5c3f4574ec36bb2ea2d2a3b44183670
radwaadel17 <radwaadel959@gmail.com>
2025-09-23 22:46:51 +0300

    feat(auth): implement Faluire class and ServerFaluire for error handling in Firebase Authentication

---
+++ app/lib/core/errors/faluire_class.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class Faluire{
+   final String message;
+   Faluire(this.message);
+
+}
+ class ServerFaluire extends Faluire{
+  ServerFaluire(super.message);
+
+  factory ServerFaluire.fromFirebaseException(FirebaseAuthException e){
+
+    switch (e.code){
+      case 'weak-password':
+        return ServerFaluire('The password provided is too weak.');
+      case 'email-already-in-use':
+        return ServerFaluire('This email address is already in use.');
+      case 'invalid-email':
+        return ServerFaluire('Your email address appears to be malformed.');
+      case 'wrong-password':
+        return ServerFaluire('Your password is wrong.');
+      case 'user-not-found':
+        return ServerFaluire("User with this email doesn't exist.");
+      case 'user-disabled':
+        return ServerFaluire('User with this email has been disabled.');
+      case 'too-many-requests':
+        return ServerFaluire('Too many requests. Try again later.');
+      case 'operation-not-allowed':
+        return ServerFaluire('Signing in with Email and Password is not enabled.');
+      default:
+        return ServerFaluire('An undefined Error happened , try again later.');
+    }
+  }
+ }
